dc79569acf783a322c526f97078e0e002f0aaec2
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-11 00:08:15 +0300

    created manage.py to ensure our unnittest is running

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import config_options
 
-#bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
@@ -14,7 +14,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
-    #bootstrap.init_app(app)
+    bootstrap.init_app(app)
 
     #Registering the blueprint
     from .main import main as main_blueprint
