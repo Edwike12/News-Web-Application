c06b63521eada582a0947604974f3c980d6f5d65
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-10 12:56:36 +0300

    added dynamic routes in views.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from config import config_options
 
 
 # Initializing application
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(config_options[config_name])
 
 
 from app import views
